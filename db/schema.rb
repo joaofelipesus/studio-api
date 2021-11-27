a89ac62281dca414d56bb225668281f51a60610d
joaofelipe <joaofelipe1294@gmail.com>
2021-11-27 11:35:22 -0300

    feat: add model muscular group.

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_127_143_045) do
+ActiveRecord::Schema.define(version: 20_211_127_143_444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
+
+  create_table 'muscular_groups', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
 end
