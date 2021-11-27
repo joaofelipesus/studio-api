47d605396f3334937e08b599c81b7f6a43c74ead
joaofelipe <joaofelipe1294@gmail.com>
2021-11-27 11:37:45 -0300

    test: add model coverage.

--- spec/models/muscular_group_spec.rb
+++ spec/models/muscular_group_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe MuscularGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 end
