f190a1819f38b9bef7e558aa9b6e4ae7a0509da8
joaofelipe <joaofelipe1294@gmail.com>
2021-11-27 11:45:40 -0300

    chore: add seeds and update erd.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# NOTE: muscular group seeds.
+MuscularGroup.create(
+  [
+    { name: 'Peitoral' },
+    { name: 'Dorsais' },
+    { name: 'Deltóides' },
+    { name: 'Trapézio' },
+    { name: 'Tríceps' },
+    { name: 'Bíceps' },
+    { name: 'Antebraço' },
+    { name: 'Coxas' },
+    { name: 'Glúteos' },
+    { name: 'Panturrilha' },
+    { name: 'Abdomên' }
+  ]
+)
