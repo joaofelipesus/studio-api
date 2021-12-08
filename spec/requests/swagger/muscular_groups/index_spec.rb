038fb1eb122e27f669685343c83a82a6aee117a3
joaofelipe <joaofelipe1294@gmail.com>
2021-12-08 09:55:01 -0300

    docs: update swagger docs.

---
+++ spec/requests/swagger/muscular_groups/index_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'swagger_helper'
+
+RSpec.describe 'List muscular_groups' do
+  let!(:current_user) { create(:user, :personal) }
+
+  path '/api/muscular_groups' do
+    get('List muscular groups') do
+      tags 'Muscular Group'
+      consumes 'application/json'
+
+      parameter(
+        in: :header,
+        type: :string,
+        name: :Authorization,
+        required: true,
+        description: 'Auth token'
+      )
+
+      security [Bearer: {}]
+
+      response(200, 'successful') do
+        let(:Authorization) { headers(user: current_user) }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
