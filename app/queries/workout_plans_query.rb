fb34ed11faa5446646c4f6841d30af3970a13d96
Joao Felipe <joaofelipe1294@gmail.com>
2022-07-10 17:47:51 -0300

    feat: add workout_plan_query object.

---
+++ app/queries/workout_plans_query.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class WorkoutPlansQuery < BaseQuery
+  def base_relation
+    WorkoutPlan.all
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def call
+    joins
+    apply_filters
+    order
+    paginate
+    {
+      workout_plans: @relation,
+      meta: {
+        total_pages:,
+        current_page:
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  attr_reader :params
+
+  def joins
+    @relation = @relation.includes(:exercise_workout_plans)
+  end
+
+  def apply_filters
+    filter_by_name
+    filter_by_personal
+  end
+
+  def filter_by_personal
+    return if params[:personal_id].blank?
+
+    @relation = @relation.where(personal_id: params[:personal_id])
+  end
+
+  def order
+    @relation = @relation.order(name: :asc)
+  end
+
+  def filter_by_name
+    return if params[:name].blank?
+
+    @relation = @relation.where('workout_plans.name ILIKE ?', "%#{params[:name]}%")
+  end
+end
