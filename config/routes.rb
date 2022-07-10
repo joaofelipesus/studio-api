cd427ba160ae27ff102604ce0a6385e290dfe561
Joao Felipe <joaofelipe1294@gmail.com>
2022-07-10 13:33:20 -0300

    feat: add delete student route.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :exercises, only: %i[create update show index destroy]
     resources :workout_plans, only: %i[create update show index]
     resources :exercise_workout_plans, only: %i[create update show index]
-    resources :students, only: %i[create update show index]
+    resources :students, only: %i[create update show index destroy]
     resources :schedules, only: %i[create update show index]
     resources :muscular_groups, only: %i[index]
   end
