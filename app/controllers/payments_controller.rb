05d55d98df5b914a17b8841bab734eb3dececc28
joaofelipe <joaofelipe1294@gmail.com>
2022-10-14 22:50:32 -0300

    feat: add payment routes

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class PaymentsController < ApplicationController
+  include Secure
+
+  def index
+    render('payments/index', formats: :json, locals: { paginated_data: payments })
+  end
+
+  def show
+    payment = Payment.find_by!(id: params[:id], personal_id: current_personal.id)
+    render_success(payment)
+  end
+
+  def create
+    payment = Payments::CreateService.call(payments_params)
+    if payment.id
+      render_success(payment, status: :created)
+    else
+      render_error_messages(payment)
+    end
+  end
+
+  def destroy
+    Payments::RemoveService.call(params[:id])
+    render status: :ok, json: {}
+  end
+
+  private
+
+  def payments_params
+    params.permit(
+      :student_plan_id,
+      :date,
+      :amount,
+      :payment_method
+    ).merge(personal_id: current_personal.id)
+  end
+
+  def payments
+    PaymentsQuery.call(
+      params: {
+        page: params[:page],
+        personal_id: current_personal.id,
+        student_plan_id: params[:student_plan_id],
+        all: params[:all]
+      }
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,12 @@ Rails.application.routes.draw do
     resources :objectives, only: %i[index]
     resources :exercises_groups, only: %i[create destroy show]
     resources :plans, only: %i[create update index show]
+    resources :payments, only: %i[create destroy index show]
     resources :student_plans, only: %i[create update] do
-      get 'student/:student_id', to: 'student_plans#student_plans', as: :student_plans,
-                                 on: :collection
+      get 'student/:student_id',
+          to: 'student_plans#student_plans',
+          as: :student_plans,
+          on: :collection
     end
   end
 end
